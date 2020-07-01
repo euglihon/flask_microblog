2ab2f610306e7a8824aeeb7096b7e4d300945305
Eugene Lihon <eug.lihon@hotmail.com>
2020-07-01 21:26:52 +0200

    Добавил функцию хеширования пароля в таблице User

--- app/models.py
+++ app/models.py
@@ -1,17 +1,38 @@
 from app import db
 from datetime import datetime
 
+from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Model):
+from flask_login import UserMixin
+from app import login
+
+
+@login.user_loader # регистрируем загрзк. пользователя (отслеживание)
+def load_user(id):
+    # делаем запрос к таблице User c id
+    return User.query.get(int(id))
+
+
+class User(UserMixin, db.Model): # add Mixin
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(64), index=True, unique=True)
+    # поле сетается из функции set_password
     password_hash = db.Column(db.String(128))
 
     # указатель на создание связи с таблицей Post и для обратной связи через таблицу User
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
+    def set_password(self, password):
+        # метод создания пароля и записи хеша в таблице
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        # метод сравнения пароля с значением в хеше (True/False)
+        return check_password_hash(self.password_hash, password)
+
     def __repr__(self):
-        return f'<User {self.username}'
+        return f'<User {self.username}>'
 
 
 class Post(db.Model):
